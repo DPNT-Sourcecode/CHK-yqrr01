846c20d867a97d2d111c9329e4004d24e03d8a4a
sbx_user1060 <sbx_user1060@ip-10-129-208-181.eu-west-2.compute.internal>
2019-03-09 17:41:11 +0000

    Sat Mar 09 17:41:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,8 +9,22 @@ def checkout(skus):
     NB: String containing the SKUs of all the products in the basket
     """
     toral = 0
+    try:
+        for sku in skus:
+            _sku = sku.lower()
+            if _sku == 'a':
+            if _sku == 'b':
+            if _sku == 'c':
+            if _sku == 'd':
+            else:
+                total = -1
+                break
 
-
+    # TODO: attr error
+    except SyntaxError as ex:
+        total = -1
+        print(str(ex))                       
 
     return total
+
 
