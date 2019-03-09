28c003ea4da56046969350ef02c382bf1ca32fe3
sbx_user1060 <sbx_user1060@ip-10-129-208-181.eu-west-2.compute.internal>
2019-03-09 17:38:11 +0000

    Sat Mar 09 17:38:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # raise NotImplementedError()
+    """
+    Docstring ...
 
+    NB: String containing the SKUs of all the products in the basket
+    """
     toral = 0
 
-    
+
 
     return total
+
