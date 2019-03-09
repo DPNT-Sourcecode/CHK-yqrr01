a6b3ec24cee382ed4791b98ddce28dcecd9f0710
sbx_user1060 <sbx_user1060@ip-10-129-208-181.eu-west-2.compute.internal>
2019-03-09 17:35:11 +0000

    Sat Mar 09 17:35:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,43 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    """
-    Docstring ...
+    # raise NotImplementedError()
 
-    NB: String containing the SKUs of all the products in the basket
-    """
-    total = 0
-    a = b = c = d = 0
+    toral = 0
 
-    try:
-        for sku in skus:
-            _sku = sku.lower()
-            if _sku == 'a':
-                a += 1
-            if _sku == 'b':
-                b += 1
-            if _sku == 'c':
-                c += 1
-            if _sku == 'd':
-                d += 1
-            else:
-                total = -1
-                raise SyntaxError
-    # TODO: attr error
-    except SyntaxError as ex:
-        total = -1
-        print(str(ex))
-    else:
-        total += (a / 3)*130 + (a % 3)*50
-        total += (b / 2)*45 + (b % 2)*30
-        total += c*20
-        total += d*15
+    
 
     return total
-
-
-
-
-
-
-
