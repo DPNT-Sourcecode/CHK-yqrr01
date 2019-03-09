83587e71c3c18dccb2ac313f808a5e0971c70da0
sbx_user1060 <sbx_user1060@ip-10-129-208-181.eu-west-2.compute.internal>
2019-03-09 17:44:11 +0000

    Sat Mar 09 17:44:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,22 +9,31 @@ def checkout(skus):
     NB: String containing the SKUs of all the products in the basket
     """
     toral = 0
+    a = b = c = d = 0
+
     try:
         for sku in skus:
             _sku = sku.lower()
             if _sku == 'a':
+                a += 1
             if _sku == 'b':
+                b += 1
             if _sku == 'c':
+                b += 1
             if _sku == 'd':
+                d += 1
             else:
                 total = -1
                 break
-
     # TODO: attr error
     except SyntaxError as ex:
         total = -1
-        print(str(ex))                       
+        print(str(ex))
+    else:
+        for item in xrange(a):
+                            
 
     return total
+
 
 
